588baae49104b5ca058b4f120c71cdd2ec8dd154
Jichun Li <jl12182@nyu.edu>
2021-10-06 17:34:02 -0400

    working on account app

--- VMentalHealth/settings.py
+++ VMentalHealth/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # third party
+    'djangorestframework',
+
+    # project app
+    'accounts',
 ]
 
 MIDDLEWARE = [
---
+++ accounts/api/views.py
@@ -0,0 +1,43 @@
+from accounts.api import serializers
+from accounts.api.serializers import UserSerializer
+from django.contrib.auth.models import User
+from rest_framework import permissions
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework.permissions import AllowAny
+from django.contrib.auth import (
+    authenticate as django_authenticate,
+    login as django_login,
+    logout as django_logout,
+)
+from accounts.api.serializers import SignupSerializer, LoginSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+
+    @action(methods=['POST'], detail=False)
+    def signup(self, request):
+        """
+        Register account with username, email and password
+        """
+        serializer = SignupSerializer(data=request.data)
+        if not serializer.is_valid():
+            return Response({
+                'success': False,
+                'message': "Please check input",
+                'errors': serializer.errors,
+            }, status=400)
+
+        user = serializer.save()
+        django_login(request, user)
+        return Response({
+            'success': True,
+            'user': UserSerializer(user).data,
+        })
