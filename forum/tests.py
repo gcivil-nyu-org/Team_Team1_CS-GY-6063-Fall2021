2794c7fefd840d7f70a8fe29613da278a5e98b7f
Jichun Li <jl12182@nyu.edu>
2021-10-19 15:33:54 -0400

    fix typo

--- forum/tests.py
+++ forum/tests.py
@@ -5,12 +5,7 @@ from datetime import timedelta
 from utils.time_helpers import utc_now
 
 
-<<<<<<< HEAD
 class PostTests(TestCase):
-=======
-class TweetTests(TestCase):
->>>>>>> 74cc855 (add post model and test case)
-
     def test_hours_to_now(self):
         test_user = User.objects.create_user(username='test_user')
         post = Post.objects.create(
@@ -21,7 +16,3 @@ class TweetTests(TestCase):
         post.created_at = utc_now() - timedelta(hours=10)
         post.save()
         self.assertEqual(post.hours_to_now, 10)
-<<<<<<< HEAD
-=======
-    
->>>>>>> 74cc855 (add post model and test case)
