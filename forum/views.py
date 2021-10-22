a21a113d08cd1299b6ec5dc4f06d54537a7bcaad
Jichun Li <jl12182@nyu.edu>
2021-10-22 17:04:55 -0400

    add post detail view

--- forum/views.py
+++ forum/views.py
@@ -1,12 +1,15 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from forum.models import Post
 
 
 class ForumView(ListView, LoginRequiredMixin):
     model = Post
-    template_name = 'forum.html'
     login_url = 'login'
 
     def get_queryset(self):
         return Post.objects.all().order_by('-created_at')
+
+
+class PostDetailView(DetailView):
+    model = Post
