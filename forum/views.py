0ed4be87743a2fe930fabc106c06fb2e1b13ae7a
Jichun Li <jl12182@nyu.edu>
2021-10-22 16:46:30 -0400

    add created post list view

--- forum/views.py
+++ forum/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from forum.models import Post
 
-# Create your views here.
+
+class ForumView(ListView, LoginRequiredMixin):
+    model = Post
+    template_name = 'forum.html'
+    login_url = 'login'
+
+    def get_queryset(self):
+        return Post.objects.all().order_by('-created_at')
