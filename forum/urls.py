1cb8b34275da21e7980a00ed7cf587837cdf6d7c
Jichun Li <jl12182@nyu.edu>
2021-10-22 17:10:27 -0400

    finished post detail view

--- forum/urls.py
+++ forum/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from forum.views import ForumView, PostDetailView
+from forum.views import PostDetailView, PostListView
 
 urlpatterns = [
-    path('', ForumView.as_view(), name='post_list'),
-    path('<int:pk>', PostDetailView.as_view(), name='post_detail')
+    path('', PostListView.as_view(), name='post_list'),
+    path('<int:pk>', PostDetailView.as_view(), name='post_detail'),
 ]
