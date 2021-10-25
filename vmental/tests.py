b8e27fc5af25310987b551be1eab04cc3e1d7896
Pragdheeshnyu <71206520+Pragdheeshnyu@users.noreply.github.com>
2021-10-24 21:48:50 -0400

    Changed a bit in test

--- vmental/tests.py
+++ vmental/tests.py
@@ -1,9 +1,8 @@
-from django.test import TestCase, client
-import datetime
-from django.utils import timezone
+from django.test import TestCase
 from django.urls import reverse
-from vmental.models import *
-from vmental.views import *
+
+# from vmental.models import *
+# from vmental.views import *
 
 
 # Create your tests here.
@@ -23,12 +22,6 @@ class VMMentalHealthTest(TestCase):
         response = self.client.get("/auth/login/")
         self.assertEqual(response.status_code, 200)
 
-        CustomizedUser.objects.create(
-            username="test_user1",
-            password="test1234",
-            date_of_birth="1997-10-09",
-            is_provider=False,
-        )
         response = self.client.post(
             "/auth/login/",
             {"username": "test_user1", "password": "test1234"},
@@ -47,23 +40,23 @@ class VMMentalHealthTest(TestCase):
             "Please enter a correct username and password.", str(response.content)
         )
 
-    def test_html_signup_failtest(self):
-        response_wrong_email = self.client.post(
-            "/auth/signup",
-            {
-                "email": "test",
-                "username": "test123",
-                "first_name": "test",
-                "last_name": "test1234",
-                "gender": "F",
-                "date_of_birth": "03/17/1998",
-                "phone_number": 123123123,
-                "password1": "test123abc456",
-                "password2": "test123abc456",
-            },
-        )
-        # print(response_wrong_email)
-        # print(response_wrong_email.content)
+    # def test_html_signup_failtest(self):
+    #     response_wrong_email = self.client.post(
+    #         "/auth/signup",
+    #         {
+    #             "email": "test",
+    #             "username": "test123",
+    #             "first_name": "test",
+    #             "last_name": "test1234",
+    #             "gender": "F",
+    #             "date_of_birth": "03/17/1998",
+    #             "phone_number": 123123123,
+    #             "password1": "test123abc456",
+    #             "password2": "test123abc456",
+    #         },
+    #     )
+    #     print(response_wrong_email)
+    #     print(response_wrong_email.content)
 
     def test_html_signup_page(self):
         response = self.client.post(
