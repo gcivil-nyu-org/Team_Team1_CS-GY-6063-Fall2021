1adb3cbd881e743755879b00f4f8e8f6282f3aa9
Jichun Li <jl12182@nyu.edu>
2021-10-26 13:40:25 -0400

    add ajax request in view

--- forum/views.py
+++ forum/views.py
@@ -1,8 +1,9 @@
+from annoying.decorators import ajax_request
 from django.views.generic import ListView, DetailView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import CreateView, DeleteView
 from django.urls import reverse_lazy
-from forum.models import Post
+from forum.models import Post, Comment
 
 
 class PostListView(LoginRequiredMixin, ListView):
@@ -47,3 +48,33 @@ class PostCreateView(LoginRequiredMixin, CreateView):
         author = self.request.user
         form.instance.author = author
         return super(PostCreateView, self).form_valid(form)
+
+
+@ajax_request
+def addComment(request):
+    comment_text = request.POST.get('comment_text')
+    post_pk = request.POST.get('post_pk')
+    post = Post.objects.get(pk=post_pk)
+    commenter_info = {}
+
+    try:
+        comment = Comment(content=comment_text, author=request.user, post=post)
+        comment.save()
+
+        username = request.user.username
+
+        commenter_info = {
+            'username': username,
+            'comment_text': comment_text,
+        }
+
+        result = 1
+    except Exception as e:
+        print(e)
+        result = 0
+
+    return {
+        'result': result,
+        'post_pk': post_pk,
+        'commenter_info': commenter_info
+    }
