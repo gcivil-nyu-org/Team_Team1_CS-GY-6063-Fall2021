d5be8343e2717a4061753a6d4afead27107eeccc
Meng <61612823+MengZhou11@users.noreply.github.com>
2021-10-27 16:00:18 -0400

    black and flake8

--- vmental/urls.py
+++ vmental/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import include, url
+from django.conf.urls import include
 from django.urls import path
 from vmental.views import IndexView, signup
 from . import views
@@ -7,5 +7,5 @@ urlpatterns = [
     path("", IndexView.as_view(), name="index"),
     path("auth/", include("django.contrib.auth.urls")),
     path("auth/signup/", signup, name="signup"),
-    path('activate/<uidb64>/<token>/', views.activate, name='activate'),
+    path("activate/<uidb64>/<token>/", views.activate, name="activate"),
 ]
