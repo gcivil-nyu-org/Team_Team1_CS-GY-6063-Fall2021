0354a0aef9c4f753f1a674c5ab48750233a4d310
Meng <61612823+MengZhou11@users.noreply.github.com>
2021-10-26 22:57:20 -0400

    email message issue

--- vmental/urls.py
+++ vmental/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import include, url
 from django.urls import path
-from vmental.views import IndexView, activate, signup
+from vmental.views import IndexView, signup
+from . import views
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
     path("auth/", include("django.contrib.auth.urls")),
     path("auth/signup/", signup, name="signup"),
     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        activate, name='activate'),
+        views.activate, name='activate'),
 ]
