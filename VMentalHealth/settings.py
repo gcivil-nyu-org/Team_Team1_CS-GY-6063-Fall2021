1dd4d7af8985616bbb90e521c2a371574d445403
Meng <61612823+MengZhou11@users.noreply.github.com>
2021-10-27 12:33:10 -0400

    fixed forget password issue

--- VMentalHealth/settings.py
+++ VMentalHealth/settings.py
@@ -143,12 +143,10 @@ LOGOUT_REDIRECT_URL = "index"
 
 AUTH_USER_MODEL = "vmental.CustomizedUser"
 
+
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
-
-STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # STATIC_URL = "/static/"
 # STATIC_ROOT = os.path.join(BASE_DIR, "static")
@@ -156,10 +154,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # for password reset function
 # EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
 # EMAIL_FILE_PATH = str(BASE_DIR.joinpath("sent_emails"))
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = "mengznyc@gmail.com"  # your email-id
-EMAIL_HOST_PASSWORD = "meng1112"  # your password
+
+EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'mengznyc@gmail.com' #your email-id
+EMAIL_HOST_PASSWORD = 'meng1112' #your password
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
@@ -178,6 +177,12 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'mengznyc@gmail.com'
 EMAIL_HOST_PASSWORD = 'meng1112'
 EMAIL_PORT = 587
+
+# EMAIL_USE_TLS = True
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_HOST_USER = 'mengznyc@gmail.com'
+# EMAIL_HOST_PASSWORD = 'meng1112'
+# EMAIL_PORT = 587
 
 # must place in the last line
 django_heroku.settings(locals(), test_runner=False)
