aba1da35be473a4cd486a1affb360492e921338b
Jichun Li <jl12182@nyu.edu>
2021-10-27 16:32:20 -0400

    add heroku settings

--- VMentalHealth/settings.py
+++ VMentalHealth/settings.py
@@ -152,4 +152,4 @@ EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
 # must place in the last line
-# django_heroku.settings(locals(), test_runner=False)
+django_heroku.settings(locals(), test_runner=False)
