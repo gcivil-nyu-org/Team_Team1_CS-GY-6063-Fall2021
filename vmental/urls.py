6caaabd8e822da6d76939e77240fec136a7ccd23
Meng <61612823+MengZhou11@users.noreply.github.com>
2021-10-26 20:00:45 -0400

    have issue

--- vmental/urls.py
+++ vmental/urls.py
@@ -1,10 +1,11 @@
-from django.conf.urls import include
+from django.conf.urls import include, url
 from django.urls import path
-
-from vmental.views import IndexView, SignUpView
+from vmental.views import IndexView, activate, signup
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
     path("auth/", include("django.contrib.auth.urls")),
-    path("auth/signup/", SignUpView.as_view(), name="signup"),
+    path("auth/signup/", signup, name="signup"),
+    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        activate, name='activate'),
 ]
