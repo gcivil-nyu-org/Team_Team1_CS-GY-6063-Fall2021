85511e3ce2327914eb1623be42ed8292f043a066
Meng <61612823+MengZhou11@users.noreply.github.com>
2021-10-27 18:47:20 -0400

    ready to be merged to develop

--- vmental/views.py
+++ vmental/views.py
@@ -1,6 +1,5 @@
 from django.views.generic import TemplateView
 from vmental.forms import UserCreationForm
-from verify_email.email_handler import send_verification_email
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import login
@@ -58,41 +57,6 @@ def signup(request):
     return render(request, "signup.html", {"form": form})
 
 
-# class SignUpView(CreateView):
-#     form_class = UserCreationForm
-#     template_name = 'signup.html'
-#     reverse_lazy= 'login.html'
-
-
-def signup(request):
-    if request.method == "POST":
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save(commit=False)
-            user.is_active = False
-            user.save()
-            current_site = get_current_site(request)
-            message = render_to_string(
-                "acc_active_email.html",
-                {
-                    "user": user,
-                    "domain": current_site.domain,
-                    "uid": urlsafe_base64_encode(force_bytes(user.pk)),
-                    "token": account_activation_token.make_token(user),
-                },
-            )
-            mail_subject = "Activate your blog account."
-            to_email = form.cleaned_data.get("email")
-            email = EmailMessage(mail_subject, message, to=[to_email])
-            email.send()
-            return render(request, "email_verify_sent.html", {"form": form})
-
-    else:
-        form = UserCreationForm()
-
-    return render(request, "signup.html", {"form": form})
-
-
 def activate(request, uidb64, token):
     try:
         uid = force_text(urlsafe_base64_decode(uidb64))
@@ -110,25 +74,3 @@ def activate(request, uidb64, token):
         # return 'index'
     else:
         return HttpResponse("Activation link is invalid!")
-# class SignUpView(CreateView):
-#     form_class = UserCreationForm
-#     template_name = "signup.html"
-#     success_url = reverse_lazy("login")
-
-def activate(request, uidb64, token):
-    try:
-        uid = force_text(urlsafe_base64_decode(uidb64))
-        User = get_user_model()
-        user = User.objects.get(pk=uid)
-    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
-        user = None
-    if user is not None:
-        # and account_activation_token.check_token(user, token):
-        user.is_active = True
-        user.save()
-        login(request, user)
-        return render(request, "index.html")
-        # return redirect('home')
-        # return 'index'
-    else:
-        return HttpResponse('Activation link is invalid!')
