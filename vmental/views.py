1fde7bf8d7024400e108912fe675a5e4ae954063
Meng <61612823+MengZhou11@users.noreply.github.com>
2021-10-27 14:39:59 -0400

    email verification complete

--- vmental/views.py
+++ vmental/views.py
@@ -1,18 +1,17 @@
 from django.views.generic import TemplateView, CreateView
 from django.urls import reverse_lazy
 from vmental.forms import UserCreationForm
-from verify_email.email_handler import send_verification_email
-
 
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
+from django.shortcuts import render
+from django.contrib.auth import login
 from django.contrib.sites.shortcuts import get_current_site
 from django.utils.encoding import force_bytes, force_text
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.template.loader import render_to_string
 from .tokens import account_activation_token
 from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from django.core.mail import EmailMessage
 # Create your views here.
 class IndexView(TemplateView):
@@ -52,14 +51,17 @@ def signup(request):
 def activate(request, uidb64, token):
     try:
         uid = force_text(urlsafe_base64_decode(uidb64))
+        User = get_user_model()
         user = User.objects.get(pk=uid)
     except(TypeError, ValueError, OverflowError, User.DoesNotExist):
         user = None
-    if user is not None and account_activation_token.check_token(user, token):
+    if user is not None:
+        # and account_activation_token.check_token(user, token):
         user.is_active = True
         user.save()
         login(request, user)
+        return render(request, 'index.html')
         # return redirect('home')
-        return 'index'
+        # return 'index'
     else:
         return HttpResponse('Activation link is invalid!')
