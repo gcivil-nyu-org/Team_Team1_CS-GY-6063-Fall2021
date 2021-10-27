dc441d0876bf9982d5e7c463a7aadfb63e088158
Meng <61612823+MengZhou11@users.noreply.github.com>
2021-10-27 16:38:06 -0400

    Update settings.py

--- VMentalHealth/settings.py
+++ VMentalHealth/settings.py
@@ -148,9 +148,6 @@ PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
 
-# STATIC_URL = "/static/"
-# STATIC_ROOT = os.path.join(BASE_DIR, "static")
-
 # for password reset function
 # EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
 # EMAIL_FILE_PATH = str(BASE_DIR.joinpath("sent_emails"))
