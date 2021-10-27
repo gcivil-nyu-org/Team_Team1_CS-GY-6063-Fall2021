3e2fe5a26a72167f37e097c0e67c1218440bcb68
Aayushi1130 <aayushi.sikligar1130@gmail.com>
2021-10-27 16:22:30 -0400

    commit 5

--- booking/tests.py
+++ booking/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.urls import reverse
+
+from booking.models import Doctor, Appointments, Patient
+
+
+# Create your tests here.
+
+
