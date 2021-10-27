f9dbbab93f06507716f396fd1cedba48d2e16476
Meng <61612823+MengZhou11@users.noreply.github.com>
2021-10-27 14:40:09 -0400

    Update settings.py

--- VMentalHealth/settings.py
+++ VMentalHealth/settings.py
@@ -163,4 +163,4 @@ EMAIL_USE_SSL = False
 # EMAIL_PORT = 587
 
 # must place in the last line
-# django_heroku.settings(locals(), test_runner=False)
+django_heroku.settings(locals(), test_runner=False)
