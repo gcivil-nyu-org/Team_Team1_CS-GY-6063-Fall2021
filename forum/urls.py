ceb367bf80c0e8ba81308a7331cd973a34c3cd7e
Jichun Li <jl12182@nyu.edu>
2021-10-30 21:40:14 -0400

    adding namespace for forum urls

--- forum/urls.py
+++ forum/urls.py
@@ -9,6 +9,8 @@ from forum.views import (
     CommentCreateView,
 )
 
+app_name = "forum"
+
 urlpatterns = [
     path("", PostListView.as_view(), name="post_list"),
     path("<int:pk>", PostDetailView.as_view(), name="post_detail"),
