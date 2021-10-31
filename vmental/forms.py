d9594c3b1ff4b28228fc8b3f79f781f03a9d8bfb
Meng <61612823+MengZhou11@users.noreply.github.com>
2021-10-30 22:19:55 -0400

    unique email address

--- vmental/forms.py
+++ vmental/forms.py
@@ -5,7 +5,6 @@ from django import forms
 
 class UserCreationForm(UserCreationForm):
     email = forms.EmailField(max_length=200, help_text="Required")
-
     class Meta(UserCreationForm.Meta):
         model = CustomizedUser
         fields = (
--- vmental/models.py
+++ vmental/models.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.models import AbstractUser
-from django.db.models.fields import BooleanField, CharField, DateField
+from django.db.models.fields import BooleanField, CharField, DateField, EmailField
 from imagekit.models import ProcessedImageField
 
 
 class CustomizedUser(AbstractUser):
+    email = EmailField(max_length=200, unique=True)
     date_of_birth = DateField(blank=True, null=True)
     GENDER_CHOICES = {
         ("M", "Male"),
