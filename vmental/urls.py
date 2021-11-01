7b4050ac33942dd8e3b02a7ee79b957ba7582519
Jichun Li <jl12182@nyu.edu>
2021-11-01 14:52:36 -0400

    remove email verification

--- vmental/urls.py
+++ vmental/urls.py
@@ -1,11 +1,17 @@
 from django.conf.urls import include
 from django.urls import path
-from vmental.views import IndexView, activate, signup
+from vmental.views import (
+    IndexView,
+    SignUpView,
+    activate,
+    # signup,
+)
 
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
     path("auth/", include("django.contrib.auth.urls")),
-    path("auth/signup/", signup, name="signup"),
+    # path("auth/signup/", signup, name="signup"),
+    path("auth/signup/", SignUpView.as_view(), name="signup"),
     path("activate/<uidb64>/<token>/", activate, name="activate"),
 ]
