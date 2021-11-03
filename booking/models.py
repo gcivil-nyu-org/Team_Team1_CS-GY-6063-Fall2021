3a1993b383850ca34cbecd6c6bc8eb067ec12078
Aayushi1130 <aayushi.sikligar1130@gmail.com>
2021-11-02 22:11:45 -0400

    bookingcommit1

--- booking/models.py
+++ booking/models.py
@@ -1,8 +1,11 @@
 from django.db import models
-
+from forum.models import Post
+#from django.contrib.auth import user_logged_in
+from vmental.models import CustomizedUser
 
 class Doctor(models.Model):  # E302
     doctor_name = models.CharField(max_length=30)
+    
 
 
 class Patient(models.Model):
@@ -15,6 +18,7 @@ class Patient(models.Model):
 class Appointments(models.Model):
     date = models.DateField()
     time = models.TimeField()
+    user = models.ForeignKey(CustomizedUser, on_delete=models.CASCADE)
     doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
 
     def __main__(self):
