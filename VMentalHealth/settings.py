d0021d67af845326e30d092648374b8ce6bf777a
Pragdheeshnyu <71206520+Pragdheeshnyu@users.noreply.github.com>
2021-11-03 18:06:14 -0400

    Update settings.py

--- VMentalHealth/settings.py
+++ VMentalHealth/settings.py
@@ -141,7 +141,7 @@ LOGIN_URL = "login"
 LOGIN_REDIRECT_URL = "index"
 
 LOGOUT_REDIRECT_URL = "index"
-ALLOWED_HOSTS = ["vmentalhealth.herokuapp.com"]
+ALLOWED_HOSTS = ["vmentalhealth.herokuapp.com", "127.0.0.1"]
 AUTH_USER_MODEL = "vmental.CustomizedUser"
 
 
