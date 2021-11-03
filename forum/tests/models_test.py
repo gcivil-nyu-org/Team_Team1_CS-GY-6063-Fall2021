a2627092e9dd1df3822bdb5927d909f00039356a
Pragdheeshnyu <71206520+Pragdheeshnyu@users.noreply.github.com>
2021-11-03 11:31:47 -0400

    Structured test cases

--- forum/tests/models_test.py
+++ forum/tests/models_test.py
@@ -81,38 +81,3 @@ class CommentModelTests(TestCase):
         )
         comment.save()
         self.assertEqual("1 test_user: 1 \n Test comment by test_user", str(comment))
-
-
-class PostListViewTests(TestCase):
-    def test_get_queryset(self):
-        test_user = CustomizedUser.objects.create_user(username="test_user")
-        post_1 = Post.objects.create(
-            author=test_user,
-            title="test_title_1",
-            content="test_content_1",
-        )
-        post_1.save()
-        post_2 = Post.objects.create(
-            author=test_user,
-            title="test_title_2",
-            content="test_content_2",
-        )
-        post_2.save()
-        post_3 = Post.objects.create(
-            author=test_user,
-            title="test_title_3",
-            content="test_content_3",
-        )
-        post_3.save()
-        request = RequestFactory().get("/forum/")
-        view = PostListView()
-        view.request = request
-        qs = view.get_queryset()
-        self.assertQuerysetEqual(qs, Post.objects.all().order_by("-created_at"))
-
-
-class CommentFormTest(TestCase):
-    def test_comment_form(self):
-        form_data = {"content": "test_content"}
-        form = CommentForm(form_data)
-        self.assertTrue(form.is_valid())
---
+++ forum/tests/views_test.py
@@ -0,0 +1,42 @@
+from vmental.models import CustomizedUser
+from django.test import TestCase, RequestFactory
+from forum.models import Post, Comment
+from forum.views import PostListView
+from forum.forms import CommentForm
+from datetime import timedelta
+from utils.time_helpers import utc_now
+
+
+class PostListViewTests(TestCase):
+    def test_get_queryset(self):
+        test_user = CustomizedUser.objects.create_user(username="test_user")
+        post_1 = Post.objects.create(
+            author=test_user,
+            title="test_title_1",
+            content="test_content_1",
+        )
+        post_1.save()
+        post_2 = Post.objects.create(
+            author=test_user,
+            title="test_title_2",
+            content="test_content_2",
+        )
+        post_2.save()
+        post_3 = Post.objects.create(
+            author=test_user,
+            title="test_title_3",
+            content="test_content_3",
+        )
+        post_3.save()
+        request = RequestFactory().get("/forum/")
+        view = PostListView()
+        view.request = request
+        qs = view.get_queryset()
+        self.assertQuerysetEqual(qs, Post.objects.all().order_by("-created_at"))
+
+
+class CommentFormTest(TestCase):
+    def test_comment_form(self):
+        form_data = {"content": "test_content"}
+        form = CommentForm(form_data)
+        self.assertTrue(form.is_valid())
