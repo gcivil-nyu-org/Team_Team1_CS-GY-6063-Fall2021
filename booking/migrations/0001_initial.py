365fedc0349097b9ed2305bd8628ca72b7e1dfc6
Pragdheeshnyu <pr2179@nyu.edu>
2021-11-10 16:12:59 -0500

    Update 0001_initial.py

--- booking/migrations/0001_initial.py
+++ booking/migrations/0001_initial.py
@@ -8,7 +8,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     initial = True
-
+    run_before = [("admin.0001_initial")]
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
