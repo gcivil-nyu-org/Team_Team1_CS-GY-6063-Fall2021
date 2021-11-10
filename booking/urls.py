3bd7e3a425cc1a9b89edb6f26e9229e58722208e
Meng <61612823+MengZhou11@users.noreply.github.com>
2021-11-10 10:13:55 -0500

    updating time slot function

--- booking/urls.py
+++ booking/urls.py
@@ -4,6 +4,7 @@ from booking.views import BookingUpdateView
 
 urlpatterns = [
     path("", views.booking, name="patient-booking"),
-    path("provider-availability", views.doctor_availability, name="provider-availability"),
+    path("provider_availability", views.timeSlotsView, name="my_availability"),
+    path("addTimeSlot", views.addSlotView, name = "addTimeSlot"),
     path("appointments/<int:pk>", BookingUpdateView.as_view(), name="appointments"),
 ]
