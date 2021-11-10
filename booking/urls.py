eec64282465e478c5bfe1e5a44a0ab1ea9edf262
Meng <61612823+MengZhou11@users.noreply.github.com>
2021-11-09 21:29:31 -0500

    update

--- booking/urls.py
+++ booking/urls.py
@@ -4,5 +4,6 @@ from booking.views import BookingUpdateView
 
 urlpatterns = [
     path("", views.booking, name="patient-booking"),
+    path("provider-availability", views.doctor_availability, name="provider-availability"),
     path("appointments/<int:pk>", BookingUpdateView.as_view(), name="appointments"),
 ]
