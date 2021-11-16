27f3dd8df15784ee1fec87e9d83fd895f72c83de
jl12182 <jl12182@nyu.edu>
2021-11-15 20:14:06 -0500

    add reserve form

---
+++ booking/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from booking.models import Appointment
+
+
+class ReserveForm(forms.ModelForm):
+    class Meta:
+        model = Appointment
+        fields = (
+            "patient",
+        )
+        widgets = {
+            "content": forms.HiddenInput(),
+        }
