3ebb3c4410845135d2de2e2446e477b604548180
Pragdheeshnyu <pr2179@nyu.edu>
2021-11-30 22:11:22 -0500

    Revert to old state

--- account/admin.py
+++ account/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from account.models import CustomizedUser
 
-admin.site.register(CustomizedUser, )
+
 @admin.register(CustomizedUser)
 class CustomizedUserAdmin(admin.ModelAdmin):
     list_display = (
