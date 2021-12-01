cf28648f3a5fa46c36dfcb862501a1152f1fc262
Pragdheeshnyu <pr2179@nyu.edu>
2021-11-30 22:08:25 -0500

    Try to fix stuff

--- account/admin.py
+++ account/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from account.models import CustomizedUser
+from django.contrib.auth.admin import UserAdmin
 
 
-@admin.register(CustomizedUser)
-class CustomizedUserAdmin(admin.ModelAdmin):
-    list_display = (
-        "id",
-        "username",
-    )
+# @admin.register(CustomizedUser)
+# class CustomizedUserAdmin(admin.ModelAdmin):
+#     list_display = (
+#         "id",
+#         "username",
+#     )
+admin.site.register(CustomizedUser, UserAdmin)
