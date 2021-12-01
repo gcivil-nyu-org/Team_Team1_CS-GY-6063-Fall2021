c8e9a0eee9713e916879874ad5c954240d60b923
Pragdheeshnyu <pr2179@nyu.edu>
2021-11-30 22:10:02 -0500

    Update admin.py

--- account/admin.py
+++ account/admin.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
 from account.models import CustomizedUser
-from django.contrib.auth.admin import UserAdmin
 
-
-# @admin.register(CustomizedUser)
-# class CustomizedUserAdmin(admin.ModelAdmin):
-#     list_display = (
-#         "id",
-#         "username",
-#     )
-admin.site.register(CustomizedUser, UserAdmin)
+admin.site.register(CustomizedUser, )
+@admin.register(CustomizedUser)
+class CustomizedUserAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "username",
+    )
