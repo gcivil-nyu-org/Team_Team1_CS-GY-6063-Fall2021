f9fd4b83865e426865432672d29e5270c1ccfb54
jl12182 <jl12182@nyu.edu>
2021-12-08 17:33:43 -0500

    fix black breaking celery

--- VMentalHealth/settings.py
+++ VMentalHealth/settings.py
@@ -166,9 +166,7 @@ EMAIL_USE_SSL = False
 CELERY_RESULT_BACKEND = "django-db"
 CELERY_CACHE_BACKEND = "django-cache"
 
-CELERY_BROKER_URL = (
-    "amqps://fgmleaxv:TsAnfCqlAeSAjRHbYXmWKrKe8RfoI5R-@fox.rmq.cloudamqp.com/fgmleaxv"
-)
+CELERY_BROKER_URL = "amqps://fgmleaxv:TsAnfCqlAeSAjRHbYXmWKrKe8RfoI5R-@fox.rmq.cloudamqp.com/fgmleaxv"
 
 BROKER_URL = "amqps://fgmleaxv:TsAnfCqlAeSAjRHbYXmWKrKe8RfoI5R-@fox.rmq.cloudamqp.com/fgmleaxv"
 
