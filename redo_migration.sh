9ef3356776900054282c8245889a58d81a38fa53
jl12182 <jl12182@nyu.edu>
2021-12-14 18:17:17 -0500

    remove phonenumber not null constraint

---
+++ redo_migration.sh
@@ -0,0 +1,6 @@
+# run the following command in terminal to redo migrations
+# ./redo_migration.sh
+find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
+find . -path "*/migrations/*.pyc"  -delete
+python manage.py makemigrations
+python manage.py migrate
