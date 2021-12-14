e080eeebc897cc883c93c04974ef2f8b7835e267
Meng <61612823+MengZhou11@users.noreply.github.com>
2021-12-14 17:52:25 -0500

    Update models.py

--- account/models.py
+++ account/models.py
@@ -17,7 +17,7 @@ class CustomizedUser(AbstractUser):
     }
     gender = CharField(max_length=1, choices=GENDER_CHOICES, blank=True)
     is_provider = BooleanField(default=False)
-    phone_number = PhoneNumberField(max_length=10, blank=True, null=True, region="US")
+    phone_number = PhoneNumberField(max_length=10, blank=True)
     profile_img = ProcessedImageField(
         upload_to="static/images/patient/profile_img",
         format="JPEG",
