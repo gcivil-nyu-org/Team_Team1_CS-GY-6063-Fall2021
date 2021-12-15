eebeac8afb26da352ff9c9dff0e67327c5619b43
dw2664 <wangdn90@gmail.com>
2021-12-14 22:56:30 -0500

    profile reform

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,18 +1,44 @@
 from datetime import date
 from django.core.validators import MaxValueValidator
 from django.db.models.fields import DateField
-from django.views.generic import TemplateView, UpdateView
+from django.views.generic import UpdateView, ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from account.models import CustomizedUser
 
-# from booking.models import Appointment
-# from forum.models import Post
+from booking.models import Appointment
+from forum.models import Post
 
 
-class ProfileView(LoginRequiredMixin, TemplateView):
+class ProfileView(LoginRequiredMixin, ListView):
     template_name = "profile.html"
     model = CustomizedUser
+    context_object_name = "data"
+
+    def get_queryset(self):
+        if self.request.user.is_provider:
+            queryset = {
+                "upcoming_appointment": Appointment.objects.filter(
+                    doctor=self.request.user.id,
+                    status="active",
+                ).order_by("-date")[:3],
+                "newest_post": Post.objects.filter(status="published",).order_by(
+                    "-created_at"
+                )[:3],
+            }
+
+        else:
+            queryset = {
+                "upcoming_appointment": Appointment.objects.filter(
+                    patient=self.request.user.id,
+                    status="active",
+                ).order_by("-date")[:3],
+                "newest_post": Post.objects.filter(status="published",).order_by(
+                    "-created_at"
+                )[:3],
+            }
+
+        return queryset
 
 
 class ProfileUpdateView(LoginRequiredMixin, UpdateView):
